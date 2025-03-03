d5e1a4830b236607d4436d0f89b2187738370af6
jayden <alien01918282@gmail.com>
2025-03-03 15:12:39 +0900

    feat: Frontend API Connection

--- src/main/java/com/erp/oneflow/presentation/user/controller/UserController.java
+++ src/main/java/com/erp/oneflow/presentation/user/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.erp.oneflow.presentation.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "백엔드 연결 성공";
+    }
+
 }
